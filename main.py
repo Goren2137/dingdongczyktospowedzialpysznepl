2799b63fa5889a2848fd97e3a4a55cf73b4ab0d0
Goren2137 <124902550+Goren2137@users.noreply.github.com>
2023-02-16 16:22:25 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+#x="programowanie po polsku"
+#print(x) #pisanie
+#print(len(x)) #długość zmiennej
+#print(x[0]) #wypisanie konkretnego znaku(0,n-1)
+#print(x[1:5])  #wypisanie od 1 do 5
+#print(x[-1]) #wypisanie od 1 do końca
+#print(x[:])
+x="Goran"
+y="Pangełow"
+print(x+" "+ y)
+print(x*3,y)
+print(x*3,y, end="ddddd") #nadpisywanie endspace
+print(x*3,y)
+print(x*3,y)
+print(x*3,y)
+print(x*3,y)
+print(f"Witaj {x} {y} ashkaihas") #formatowany string
+#x=input("Podaj swoj wiek")
+#print(int(x)+2)
+james_bond=7
+#nasz cel to 007
+print(str(james_bond).zfill(3))
